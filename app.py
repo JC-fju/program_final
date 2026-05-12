cd204996bf62195215868ea9a22b052f661e7eaf
JC-fju <414170443@m365.fju.edu.tw>
2026-05-12 22:48:23 +0800

    主視覺

--- app.py
+++ app.py
@@ -5,6 +5,18 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/achievements")
+def achievements():
+    return render_template("achievements.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
 
     app.run(host="0.0.0.0",debug=True, port=5000)
