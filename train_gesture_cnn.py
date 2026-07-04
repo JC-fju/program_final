78956455307b69dc66a8418931415c6dcf687e4d
JC-fju <414170443@m365.fju.edu.tw>
2026-07-04 16:02:52 +0800

    last-1

--- train_gesture_cnn.py
+++ train_gesture_cnn.py
@@ -285,9 +285,9 @@ def main():
     class_weight = {
         0: 1.1,
         1: 1.0,
-        2: 1.3,
-        3: 1.3,
-        4: 1.3,
+        2: 1.0,
+        3: 1.0,
+        4: 1.0,
         5: 1.1,
     }
     print("⚖️  Class weights:", class_weight)
